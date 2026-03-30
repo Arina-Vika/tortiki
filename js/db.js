45695c5108ef289bc469c0fdcfc8dd1524f6ab27
Arino4ka2MV4 <schevelewaarina@yandex.ru>
2026-03-30 20:19:55 +0300

    itog

---
+++ js/db.js
@@ -0,0 +1,39 @@
+var supabaseUrl = "https://goocvassrianqsildbci.supabase.co";
+var supabaseKey = "sb_publishable_8Ep3MbhG3IZYcK1x_BwVKA_TTxN-rNt";
+
+var container = document.querySelector(".card-container");
+
+var supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+async function loadCakes() {
+    let {data, err} = await supabase.from('cakes').select('*');
+
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    data.forEach(cake => {
+        const card = document.createElement("div");
+        card.setAttribute("data-aos", "zoom-in");
+        card.setAttribute("data-aos-duration", "300");
+        card.classList.add("card");
+
+        if (cake.isPiece = false) {
+            card.innerHTML = `
+            <img src="${cake.image}" alt="${cake.name}">
+            <h2 class="cake-name">${cake.name}</h2>
+            <p class="cake-price">${cake.price}₽/кг.</p>
+        `;
+        } else {
+            card.innerHTML = `
+            <img src="${cake.image}" alt="${cake.name}">
+            <h2 class="cake-name">${cake.name}</h2>
+            <p class="cake-price">${cake.price.toLocaleString()}₽/уп.</p>
+        `;
+        }
+        container.appendChild(card);
+    });
+}
+
+loadCakes();
---
+++ js/script.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('.order-btn2');
+const nameInput = document.querySelector('.client-name');
+const phoneInput = document.querySelector('.client-phone');
+const msgInput = document.querySelector('.client-msg');
+const invalidForm = document.querySelector('.invalid-form');
+const orderConfirm = document.querySelector('.order-confirm');
+const feedbackHeader = document.querySelector('.feedback-header');
+
+const burger = document.querySelector('.burger');
+
+const menuList = document.querySelector('.menu-list');
+
+function showMenu() {
+    
+}
+
+burger.addEventListener('click', () => {
+    if (menuList.classList.contains('menu-show')) {
+        menuList.classList.remove('menu-show');
+    } else {
+        menuList.classList.add('menu-show');
+    }
+})
+
+function clearInputs() {
+    nameInput.value = "";
+    phoneInput.value = "";
+    msgInput.value = "";
+}
+
+if(btn) {
+    btn.addEventListener('click', () => {
+        if (nameInput.value && phoneInput.value && msgInput.value) {
+            emailjs.send('service_tmww8f4', 'template_vbqnn1h', 
+                {name: nameInput.value, 
+                message: msgInput.value, 
+                phone: phoneInput.value}).then((res) => {
+                    clearInputs();
+                    feedbackHeader.classList.add('hidden');
+                    setTimeout(() => {
+                        feedbackHeader.classList.remove('hidden');
+                    }, 3000);
+                    orderConfirm.classList.add('show');
+                    setTimeout(() => {
+                        orderConfirm.classList.remove('show');
+                    }, 3000);
+                }, (err) => {
+                    console.log('FAILED', err.text);
+                })
+        } else {
+            invalidForm.classList.add('active');
+            setTimeout(() => {
+                invalidForm.classList.remove('active');
+            }, 3000);
+        }
+    })
+}
+
